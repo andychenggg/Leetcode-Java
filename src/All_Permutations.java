74d3e0cc34c87ed3defc70e264ad4eba0dc9a950
andy <2768054177@qq.com>
2025-04-05 02:06:19 +0800

    First Commit

---
+++ src/All_Permutations.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class All_Permutations {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    List<List<Integer>> allPermutations(List<Integer> nums) {
+        return null;
+    }
+}
