a98677b4b2f43454f67618594810e3efa6618d48
andy <2768054177@qq.com>
2025-04-06 00:51:47 +0800

    move from idea to vscode

--- All_Permutations.java
+++ All_Permutations.java
@@ -4,10 +4,12 @@ public class All_Permutations {
 
 
     public static void main(String[] args) {
-
+        System.out.println("Hello World");
     }
 
     List<List<Integer>> allPermutations(List<Integer> nums) {
         return null;
+
+
     }
 }
