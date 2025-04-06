24f9b66c036a6b446b3f93ba1299e14e6dc601ad
andy <2768054177@qq.com>
2025-04-07 02:32:05 +0800

    Codetop1, Leetcode3

---
+++ CodeTop1.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CodeTop1 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * CodeTop1, Leetcode3: https://leetcode.cn/problems/longest-substring-without-repeating-characters/description/
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        char[] str = s.toCharArray();
+        int resLen = 0;
+        // use slide window(2-pointer) to solve.
+        int p1 = 0, p2 = 0;
+        Set<Character> charSet = new HashSet<>();
+        while(p2 != str.length){
+            // Calculate the max len from index p1
+            while(p2 != str.length && !charSet.contains(str[p2])){
+                charSet.add(str[p2]);
+                p2++;
+            }
+            if(charSet.size() > resLen)
+                resLen = charSet.size();
+            charSet.remove(str[p1++]);
+            if(p2 < p1)
+                p2 = p1;
+        }
+        return resLen;
+    }
+}
