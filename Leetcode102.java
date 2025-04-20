995a6c983a3ea43141ec3f70dddad63f91dea052
andy <2768054177@qq.com>
2025-04-21 01:26:51 +0800

    Leetcode102

---
+++ Leetcode102.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode102 {
+
+    public static void main(String[] args) {
+        
+    }
+}
+class TreeNode {
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        traversal(root, 0, res);
+        return res;
+    }
+
+    public void traversal(TreeNode root, int level, List<List<Integer>> res){
+        if (root == null){
+            return;
+        }
+        if(res.size() > level){
+            res.get(level).add(root.val);
+        }
+        else{
+            res.add(new ArrayList<>());
+            res.get(level).add(root.val);
+        }
+        traversal(root.left, level+1, res);
+        traversal(root.right, level+1, res);
+    }
+}
