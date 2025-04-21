a17b957b5cb8b191077137412f822dc7b6a25319
andy <2768054177@qq.com>
2025-04-21 17:47:05 +0800

    Leetcode88

---
+++ Leetcode88.java
@@ -0,0 +1,23 @@
+public class Leetcode88 {
+
+    public static void main(String[] args) {
+        
+    }
+}
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1, p2 = n-1, p3 = m+n-1;
+        while(p1 >=0 && p2 >=0){
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p3--] = nums1[p1--];
+            }
+            else{
+                nums1[p3--] = nums2[p2--];
+            }
+        }
+
+        while(p2 >= 0)nums1[p3--] = nums2[p2--];
+
+        
+    }
+}
