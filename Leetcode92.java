74b20ab2dde9c034acf346be8a3105c84e7b7943
andy <2768054177@qq.com>
2025-04-24 02:26:33 +0800

    Leetcode92

---
+++ Leetcode92.java
@@ -0,0 +1,32 @@
+public class Leetcode92 {
+
+    
+}
+   class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode fakeHead = new ListNode(), pre = fakeHead, tail = fakeHead;
+        fakeHead.next = head;
+
+        while(--left > 0) pre = pre.next;
+        while(right-- >= 0) tail = tail.next;
+
+        ListNode nxt = tail;
+        while(pre.next != nxt){
+            ListNode cur = pre.next;
+            pre.next = cur.next;
+            cur.next = tail;
+            tail = cur;
+        }
+        pre.next = tail;
+
+        return fakeHead.next;
+    }
+}
